3e7b58b298e53417b6bcaa1a597b0b5bb72c27c3
ayrokid <ayrokid@gmail.com>
2016-01-07 17:15:36 +0700

    sync

--- send.py
+++ send.py
@@ -8,23 +8,33 @@ passwd="root", # your password
 db="push",
 cursorclass=MySQLdb.cursors.DictCursor) # name of the data base
 
-
 credentials = ['6281390688955', 'Ga3lQvVTvzt10PlCGC/W5MAJfuE=']
 
 data = []
 
 try:
     cur = db.cursor()
-    cur.execute("select * from messages")
+    cur.execute("select id,content from messages where status='1' limit 1")
+    msg = cur.fetchone()
+    #print "Message : %s " % msg['content']
+    
+    cur.execute("select nomor from msisdn")
     results = cur.fetchall()
+
     i = 0;
     for row in results:
-       data.append([ row['sender'], row['content'] ])
+       data.append([ row['nomor'], msg['content'] ])
        i += 1
     
     #stack = sendclient.YowsupSendStack(credentials, [(['6285725523023', 'pesan dari ubuntu'])])
-    stack = sendclient.YowsupSendStack(credentials, data)
-    stack.start()
+    #stack = sendclient.YowsupSendStack(credentials, data)
+    #stack.start()
+    cur.execute("""update messages set status=0 where id=%s """, (msg['id']) )
+    db.commit()
     print('\nKirim Sukses..')
 except KeyboardInterrupt:
+    db.rollback()
     print('\nYowsdown')
+
+#disconnect from server
+db.close()
